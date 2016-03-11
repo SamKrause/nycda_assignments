2876c906275e41d28928f3ff47ec458c5569162b
Sam Krause <samuelnkrause@gmail.com>
2016-03-10 23:04:50 -0500

    finished cookie project

--- basic_rails_app/app/controllers/users_controller.rb
+++ basic_rails_app/app/controllers/users_controller.rb
@@ -18,5 +18,4 @@ class UsersController < ApplicationController
       redirect_to '/users'
     end
   end
-
 end
--- basic_rails_app/app/models/user.rb
+++ basic_rails_app/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  def access
+    "You have access to the basic functions of the website. #{adminAccess}"
+  end
 end
 
 class Admin < User
