fcf69d152dd98ad8c23f94c8d452caa5f93afff1
Sam Krause <samuelnkrause@gmail.com>
2016-03-23 21:52:47 -0400

    finished parallax

--- parallax/main.js
+++ parallax/main.js
@@ -1,34 +1,32 @@
 $(function() {
 
-  var position1 = 5
-  var position2 = -5
-
   $(window).scroll(function() {
-    position1 = -$(this).scrollTop() * 2
+    position1 = $(this).scrollTop() * 1.2;
+    position2 = -$(this).scrollTop() * 1.5;
+    position3 = $(this).scrollTop() * 2;
+    position4 = -$(this).scrollTop() *3;
+    position5 = $(this).scrollTop() * 4;
+
     $('#balloon1').css({
       'transform' : 'translateY(' + position1.toString() + 'px)'
     });
-    position2 = $(this).scrollTop() * 1.5
+
     $('#balloon2').css({
       'transform' : 'translateY(' + position2.toString() + 'px)'
     });
-  //   $('#balloon3').css({
-  //     'transform' : 'translateY(' + position2.toString() + 'px)'
-  //   });
-  //   position2 -= 1;
-  //   $('#balloon4').css({
-  //     'transform' : 'translateY(' + position2.toString() + 'px)'
-  //   });
-  //   position2 -= 1;
-  });
 
-  $('#balloon1').click(function(){
-    console.log("click");
-    $('#balloon1').css({
-      'transform' : 'translateY(200px)'
+    $('#balloon3').css({
+      'transform' : 'translateY(' + position3.toString() + 'px)'
+    });
+
+    $('#balloon4').css({
+      'transform' : 'translateY(' + position4.toString() + 'px)'
+    });
+
+    $('#balloon5').css({
+      'transform' : 'translateY(' + position5.toString() + 'px)'
     });
   });
-
 
 
 });
